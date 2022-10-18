61497ed207c8a124ad0ea8e731d8f833d8c1e8d0
Sogolon <sogolonmvj@yahoo.com>
2022-10-18 18:35:36 -0300

    Primeiro commit!

---
+++ src/main/java/com/letscode/agrocinetickets/Sessionmicroservice/SessionMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.letscode.agrocinetickets.Sessionmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/letscode/agrocinetickets/Sessionmicroservice/SessionMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letscode.agrocinetickets.Sessionmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
