688ea830727b0339b2c80e605f947c44e059b68f
Sogolon <sogolonmvj@yahoo.com>
2022-10-18 20:08:58 -0300

    Chore: implementing service functionalities!

--- src/main/java/com/letscode/agrocinetickets/Sessionmicroservice/service/SessionService.java
+++ src/main/java/com/letscode/agrocinetickets/Sessionmicroservice/service/SessionService.java
@@ -1,2 +1,52 @@
-package com.letscode.agrocinetickets.Sessionmicroservice.service;public class SessionService {
+package com.letscode.agrocinetickets.Sessionmicroservice.service;
+
+import com.letscode.agrocinetickets.Sessionmicroservice.model.dto.SessionRequest;
+import com.letscode.agrocinetickets.Sessionmicroservice.model.dto.SessionResponse;
+import com.letscode.agrocinetickets.Sessionmicroservice.repository.SessionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class SessionService {
+
+    private final SessionRepository sessionRepository;
+
+    public Mono<SessionResponse> createSession(SessionRequest sessionRequest) {
+        // TODO sessionRepository.save(sessionRequest);
+        return null;
+    }
+
+    public Flux<SessionResponse> findSessions() {
+        // TODO sessionRepository.findAll();
+        return null;
+    }
+
+    public Mono<SessionResponse> findSession(String id) {
+        // TODO sessionRepository.findById(id);
+        return null;
+    }
+
+    public Flux<SessionResponse> findSessionsByRoom(String roomId) {
+        // TODO sessionsRepository.findByRoomId(roomId);
+        return null;
+    }
+
+    public Flux<SessionResponse> findSessionsByTime(@RequestParam("time") String timeId) {
+        // TODO sessionsRepository.findByTime(roomId);
+        return null;
+    }
+
+    public Flux<SessionResponse> findSessionsByMovie(@RequestParam("movieId") String movieId) {
+        // TODO sessionsRepository.findByRoomId(roomId);
+        return null;
+    }
+
+    public Mono<SessionResponse> modifySession(String id) {
+        //TODO SessionsRepository.modifySession(id)
+        return null;
+    }
 }
