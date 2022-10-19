cc876a2c086ba123ee841aaba9634a1493e42dbb
Sogolon <sogolonmvj@yahoo.com>
2022-10-18 21:02:08 -0300

    Chore: implementing endpoints!

--- src/main/java/com/letscode/agrocinetickets/Sessionmicroservice/controller/SessionController.java
+++ src/main/java/com/letscode/agrocinetickets/Sessionmicroservice/controller/SessionController.java
@@ -6,6 +6,8 @@ import com.letscode.agrocinetickets.Sessionmicroservice.service.SessionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/sessions")
@@ -15,54 +17,68 @@ public class SessionController {
     private final SessionService sessionService;
 
     @PostMapping
-    public ResponseEntity<SessionResponse> createSession(@RequestBody SessionRequest sessionRequest) {
-
+    public ResponseEntity<Mono<SessionResponse>> createSession(@RequestBody SessionRequest sessionRequest) {
+        return null;
     }
 
     @GetMapping
-    public ResponseEntity<SessionResponse> findSessions() {
-
+    public ResponseEntity<Flux<SessionResponse>> retrieveSessions() {
+        return null;
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<SessionResponse> findSession(@PathVariable("id") String id) {
-
+    public ResponseEntity<Mono<SessionResponse>> findSession(@PathVariable("id") String id) {
+        return null;
     }
 
     @GetMapping
-    public ResponseEntity<SessionResponse> findSessionsByRoom(@RequestParam("roomId") String roomId) {
-
+    public ResponseEntity<Flux<SessionResponse>> findSessionsByRoom(@RequestParam("roomId") String roomId) {
+        return null;
     }
 
     @GetMapping
-    public ResponseEntity<SessionResponse> findSessionsByTime(@RequestParam("time") String timeId) {
-
+    public ResponseEntity<Flux<SessionResponse>> findSessionsByTime(@RequestParam("time") String timeId) {
+        return null;
     }
 
     @GetMapping
-    public ResponseEntity<SessionResponse> findSessionsByMovie(@RequestParam("movieId") String movieId) {
-
+    public ResponseEntity<Flux<SessionResponse>> findSessionsByMovie(@RequestParam("movieId") String movieId) {
+        return null;
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<SessionResponse> modifySession(@PathVariable("id") String id) {
-
+    public ResponseEntity<Mono<SessionResponse>> modifySession(@PathVariable("id") String id) {
+        return null;
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<SessionResponse> modifySessionEntirely(@PathVariable("id") String id) {
-
+    public ResponseEntity<Mono<SessionResponse>> modifySessionEntirely(@PathVariable("id") String id) {
+        return null;
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<SessionResponse> deleteSession(@PathVariable("id") String id) {
-
+    public ResponseEntity<?> deleteSession(@PathVariable("id") String id) {
+        return null;
     }
 
     @PatchMapping("/{id}/occupySeat")
-    public ResponseEntity<SessionResponse> occupySeat(@PathVariable("id") String id, @RequestParam("line") int line, @RequestParam("column") int column) {
-
+    public ResponseEntity<Mono<SessionResponse>> occupySeat(@PathVariable("id") String id, @RequestParam("line") int line, @RequestParam("column") int column) {
+        return null;
     }
 
+    @GetMapping("/{id}/isSeatOccupied")
+    public ResponseEntity<Mono<SessionResponse>> verifySeat(@PathVariable("id") String id, @RequestParam("line") int line, @RequestParam("column") int column) {
+        return null;
+    }
+
+    @PatchMapping("/{id}/vacateSeat")
+    public ResponseEntity<Mono<SessionResponse>> vacateSeat(@PathVariable("id") String id, @RequestParam("line") int line, @RequestParam("column") int column) {
+        return null;
+    }
+
+    @GetMapping("/{id}/seats")
+    public ResponseEntity<Mono<SessionResponse>> retrieveSessionSeats(@PathVariable("id") String id) {
+        return null;
+    }
 
 }
